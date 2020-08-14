d3e0324bb47041e57df24d06757ce01465cbdb7b
GradyMellin <mellingrady@gmail.com>
2020-08-14 11:43:44 -0600

    Car Insurance project pg: :346
    Car Insurance project pg: :346
    and BlackJack project

--- BlackJack/BlackJack/Program.cs
+++ BlackJack/BlackJack/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Security.Policy;
@@ -15,21 +17,65 @@ namespace BlackJack
         static void Main(string[] args)
         {
             const string casinoName = "World Underground Gambling Ring";
+
             Console.WriteLine("Welcome to the {0}, What is your name?", casinoName);
             string playerName = Console.ReadLine();
-            Console.WriteLine("Hello, {0} how much money did you bring today?", playerName);
-            int bank = Convert.ToInt32(Console.ReadLine());
+            if (playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions();
+                foreach(var exception in Exceptions)
+                {
+                    Console.Write(exception.Id + " | ");
+                    Console.Write(exception.ExceptionType + " | ");
+                    Console.Write(exception.ExceptionMessage + " | ");
+                    Console.Write(exception.TimeStamp + " | ");
+                    Console.WriteLine();
+                }
+                Console.ReadLine();
+                return;
+            }    
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("Hello, {0} how much money did you bring today?", playerName);
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) { Console.WriteLine("Please enter digits only, no decimals."); }
+            }
+            
             Console.WriteLine("{0}, would you like to play a game of BlackJack? ", playerName);
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
             {
                 Player player = new Player(playerName, bank);
+                player.Id = Guid.NewGuid();
+                using (StreamWriter file = new StreamWriter(@"C:\Users\Grady Mellin\Desktop\C#_course\TA_C#_Projects\BlackJack\log.txt", true))
+                {
+                    file.WriteLine(player.Id);
+                }
                 Game game = new BlackJackGame();
                 game += player;
                 player.isActivelyPlaying = true;
                 while(player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        UpdateDbWithExceptions(ex);
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("An error occurred. Please contact your System Administrator.");
+                        UpdateDbWithExceptions(ex);
+                        Console.ReadLine();
+                        return;
+                    } 
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
@@ -38,5 +84,57 @@ namespace BlackJack
             Console.Read();
         }
 
+        private static void UpdateDbWithExceptions(Exception ex)
+        {
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = BlackJackGame; 
+                                       Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; 
+                                       ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            string queryString = @"INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) VALUES
+                                    (@ExceptionType, @ExceptionMessage, @TimeStamp)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
+                command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", SqlDbType.DateTime);
+
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+        private static List<ExceptionEntity> ReadExceptions()
+        {
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = BlackJackGame; 
+                                       Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; 
+                                       ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            string queryString = @"SELECT Id, ExceptionType, ExceptionMessage, TimeStamp FROM Exceptions";
+
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+            using ( SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+            }
+            return Exceptions;
+        }
     }
 }
---
+++ CarInsurance/CarInsurance/Controllers/InsureeController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                Quote(insuree);
+                db.Insurees.Add(insuree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insuree);
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                Quote(insuree);
+                db.Entry(insuree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        //Calculates an Insurance quote from User Inputs
+        public ActionResult Quote(Insuree insuree)
+        {
+            //Base quote:
+            decimal quote = 50.00m;
+            //calculate age:
+            int age = 0;
+            age = DateTime.Now.Year - insuree.DateOfBirth.Year;
+            if (DateTime.Now.DayOfYear < insuree.DateOfBirth.DayOfYear)
+            { 
+                age = age - 1; 
+            }
+
+            if (age < 18)
+            {
+                quote += 100.00m;
+            }
+            if( age >= 19 && age <= 25)
+            {
+                quote += 50.00m;
+            }
+            if (age < 25)
+            {
+                quote += 25.00m;
+            }
+            if (insuree.CarYear < 2000)
+            {
+                quote += 25.0m;
+            }
+            if (insuree.CarYear > 2015)
+            {
+                quote += 25.0m;
+            }
+            if(insuree.CarMake == "Porsche")
+            {
+                quote += 25.0m;
+            }
+            if (insuree.CarMake == "Porsche" && insuree.CarModel == "911 Carrera")
+            {
+                quote += 25.0m;
+            }
+            quote += (insuree.SpeedingTickets * 10.0m);
+            if (insuree.DUI == true)
+            {
+                quote *= 1.25m;
+            }
+            if(insuree.CoverageType == true)
+            {
+                quote *= 1.5m;
+            }
+            insuree.Quote = quote;         
+            return View(insuree);
+        }
+        // GET: Admin
+        public ActionResult Admin()
+        {
+            return View(db.Insurees.ToList());
+        }
+    }
+}
